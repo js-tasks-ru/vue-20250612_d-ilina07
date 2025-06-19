eab79564016ca8bd47689031b813b96e6f183d08
Daria Ilina <d.ilina@ffintech.com>
2025-06-19 18:00:11 +0300

    createApp

--- 01-basics/10-create-app/script.js
+++ 01-basics/10-create-app/script.js
@@ -1,1 +1,24 @@
 import { defineComponent, createApp } from 'vue'
+
+const App = defineComponent({
+  name: 'App',
+
+  setup() {
+
+    const formatter = new Intl.DateTimeFormat('en-US', { dateStyle: 'long' });
+    const date = formatter.format(new Date());
+
+    return {
+      date,
+    }
+  },
+
+  template: `
+    <div>Сегодня {{ date }}</div>
+  `
+})
+
+const app = createApp(App);
+const vm = app.mount('#app');
+
+window.vm = vm;
